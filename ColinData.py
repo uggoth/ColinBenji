ea366e0441986be9a385d80873c8fc92293b303a
uggoth <panda@sidmouthrobots.co.uk>
2024-01-03 10:48:24 +0000

    major tidy up

--- ColinData.py
+++ ColinData.py
@@ -2,15 +2,15 @@ module_name = 'ColinData.py'
 
 class ColinData():
     def __init__(self):
-        self.pi_name = 'BENJI'
-        #  To obtain the port name use:    ls /dev/serial/by-id
-        self.ax12_path = '/dev/serial/by-id/usb-FTDI_USB__-__Serial_Converter_FT4TCJWH-if00-port0'
-        self.ax12_speed = 1000000
-        self.ax12_list = [20,21]
-        self.params =  {'PI_NAME':self.pi_name,
-                        'AX12_PATH':self.ax12_path,
-                        'AX12_SPEED':self.ax12_speed,
-                        'AX12_LIST':self.ax12_list}
+        self.params =  {'PI_NAME':'BENJI',
+                        'ColObjects':'ColObjects_Pi_V15',
+                        'AX12_Servo':'AX12_Servo_V01',
+                        #  To obtain the AX12 port name use:    ls /dev/serial/by-id
+                        'AX12_PATH':'/dev/serial/by-id/usb-FTDI_USB__-__Serial_Converter_FT4TCJWH-if00-port0',
+                        'AX12_SPEED':1000000,
+                        'AX12_LIST':[20,21],
+                        'BUZZER':[],
+                        'LED':[]}
 
 if __name__ == "__main__":
     print (module_name, 'starting')
--- main_test_paths.py
+++
@@ -1,6 +0,0 @@
-module_name = 'main_test_paths.py'
-print (module_name, 'starting')
-
-from importlib.machinery import SourceFileLoader
-gpio = SourceFileLoader('GPIO', '/home/pi/ColinPiClasses/GPIO_Pi_v46.py').load_module()
-fred = gpio.GPIO('dummy','INFRA_RED',17)
